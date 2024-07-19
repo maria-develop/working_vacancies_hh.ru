67718c93278128aedecedbf1ad7ee03f055587e4
Maria Ivanova <moi066@mail.ru>
2024-07-19 14:35:49 +0300

    добавлены flake8, mypy, black, isort. добавлен функционал сохранения выгруженных вакансий в /data/

--- src/hh_api.py
+++ src/hh_api.py
@@ -1,3 +1,5 @@
+import json
+import os
 from abc import ABC, abstractmethod
 import requests
 
@@ -9,12 +11,15 @@ class JobAPI(ABC):
 
 
 class HeadHunterAPI(JobAPI):
+    """Выгрузка вакансий с сайта hh.ru по api"""
     def __init__(self):
+        """Определение ресурса и параметров для api"""
         self.url = 'https://api.hh.ru/vacancies'
         self.headers = {'User-Agent': 'HH-User-Agent'}
         self.params = {'text': '', 'page': 0, 'per_page': 100}
 
     def get_vacancies(self, keyword: str):
+        """Выгрузка вакансий с проверкой статус-кода 200"""
         self.params['text'] = keyword
         vacancies = []
         for page in range(20):
@@ -25,11 +30,38 @@ class HeadHunterAPI(JobAPI):
             vacancies.extend(response.json().get('items', []))
         return vacancies
 
+
+    def save_vacancies_to_json(self, keyword: str, vacancies: list):
+        """Сохранение вакансий в JSON файл"""
+        # Определение пути к директории data в корне проекта
+        data_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'data')
+        os.makedirs(data_dir, exist_ok=True)
+        # os.makedirs('C:/Users/User/Desktop/python_rpoject_Maria/working_vacancies_hh.ru/data', exist_ok=True)
+
+        # Определите имя файла с помощью ключевого слова
+        filename = os.path.join(data_dir, f"{keyword}_vacancies.json")
+        # filename = os.path.join("C:/Users/User/Desktop/python_rpoject_Maria/working_vacancies_hh.ru/data", f"{keyword}_vacancies.json")
+
+        # Сохранение вакансий в JSON-файл
+        with open(filename, 'w', encoding='utf-8') as f:
+            json.dump(vacancies, f, ensure_ascii=False, indent=4)
+
+        print(f"Вакансии сохранены в файл {filename}")
+
+    @staticmethod
+    def get_data_file_path(filename: str) -> str:
+        """Получение абсолютного пути к файлу данных."""
+        return os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'data', filename)
+
+
 if __name__ == "__main__":
     hh_api = HeadHunterAPI()
-    keyword = "Python"  # Ваш поисковый запрос
+    keyword = "альметьевск"  # Ваш поисковый запрос
     vacancies = hh_api.get_vacancies(keyword)
 
     # Вывод списка вакансий
-    for vacancy in vacancies:
-        print(vacancy)
+    # for vacancy in vacancies:
+    #     print(vacancy)
+
+    # Сохранение вакансий в JSON-файл
+    hh_api.save_vacancies_to_json(keyword, vacancies)
